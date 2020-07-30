4fd6b826378072900af3470ad0351799268c8fb6
rtr239 <rtr239@yahoo.com>
2020-07-30 09:17:51 +0200

    created printWordText function

--- bleep.cpp
+++ bleep.cpp
@@ -1,14 +1,15 @@
 #include <iostream>
 
-int main() {
+std::string word = "broccoli";
+
+std::string text = "I love broccoli,\n"
+					"I don't see my life without broccoli.\n"
+					"Each day I eat broccoli.";
+
+
+void printWordText(std::string word, std::string text) {
 
 	std::cout << "bleep version 1 \n";
-
-	std::string word = "broccoli";
-
-	std::string text = "I love broccoli,\n"
-	"I don't see my life without broccoli.\n"
-	"Each day I eat broccoli";
 
 	std::cout << "The word to bleep is: "
 	<< '"'
@@ -21,6 +22,13 @@ int main() {
 	<< text
 	<< '"' 
 	<< "\n";
+	};
+
+
+
+int main() {
+
+	printWordText(word, text);
 
 
 }
