9809c3514f2080881ffd47d899e26da27ccc43d3
rtr239 <rtr239@yahoo.com>
2020-07-30 09:04:29 +0200

    added word and text

--- bleep.cpp
+++ bleep.cpp
@@ -4,6 +4,23 @@ int main() {
 
 	std::cout << "bleep version 1 \n";
 
+	std::string word = "broccoli";
+
+	std::string text = "I love broccoli,\n"
+	"I don't see my life without broccoli.\n"
+	"Each day I eat broccoli";
+
+	std::cout << "The word to bleep is: "
+	<< '"'
+	<< word 
+	<< '"'
+	<< "\n";
+	
+	std::cout << "The initial text is: " 
+	<< '"'	
+	<< text
+	<< '"' 
+	<< "\n";
 
 
 }
