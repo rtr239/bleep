ece850c409c9828ce74390cf68522178abc4ad78
rtr239 <rtr239@yahoo.com>
2020-07-31 07:19:10 +0200

    created finWordPos

--- bleep.cpp
+++ bleep.cpp
@@ -1,11 +1,5 @@
 #include <iostream>
-
-std::string word = "broccoli";
-
-std::string text = "I love broccoli,\n"
-					"I don't see my life without broccoli.\n"
-					"Each day I eat broccoli.";
-
+#include <string>
 
 void printWordText(std::string word, std::string text) {
 
@@ -25,10 +19,43 @@ void printWordText(std::string word, std::string text) {
 	};
 
 
+size_t finWordPos (std::string word, 
+	std::string text, size_t startPos){
+
+	size_t wordPos = text.find(word, startPos);
+
+	if(wordPos != std::string::npos){
+			std::cout << "Position of the word to bleep: " 
+			<< wordPos << "\n";
+			return wordPos;
+	} else {
+			std::cout << "Out of bounds."
+			" Please enter a positon less than the lenght"
+			" of the sentence. \n";
+			return 0;
+	};
+	
+};
+
 
 int main() {
 
+	std::string word = "broccoli";
+
+	std::string text = "I love broccoli,\n"
+					"I don't see my life without broccoli.\n"
+					"Each day I eat broccoli.";
+
+	size_t wordPos;
+	std::string bleepWord = "************************";
+
+	
 	printWordText(word, text);
+	wordPos = finWordPos(word, text, 0);
+
+
+	text.replace(wordPos, 8, bleepWord, 0, 8);
+	std::cout << text << "\n";
 
 
 }
